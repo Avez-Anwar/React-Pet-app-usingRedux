e386a64f0654dfc0f7e412ced7e7c69d82d515d4
Avez Anwar <avezanwar6@gmail.com>
2023-01-22 12:43:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
+import { petIncrement, petDecrement } from "./actions";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const petCounter = useSelector((state) => state.petCounter);
+  const petFavorite = useSelector((state) => state.petFavorite);
+  const dispatch = useDispatch();
+
+  const handlePetIncrement = () => {
+    dispatch(petIncrement(2));
+    console.log("2 sholud be passed as a payload");
+  };
+
+  const handlePetDecrement = () => {
+    dispatch(petDecrement());
+    console.log("The value get increment by actions and reducers");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pet Counter : {petCounter}</h1>
+      <h1>Pet Favorite : {petFavorite}</h1>
+      <button onClick={handlePetIncrement}>Add Pet</button>
+      <button onClick={handlePetDecrement}>Remove Pet</button>
+      {/* <button onClick={() => dispatch(petIncrement(2))}>Add Pet</button>
+      <button onClick={() => dispatch(petDecrement())}>Remove Pet</button> */}
     </div>
   );
 }
